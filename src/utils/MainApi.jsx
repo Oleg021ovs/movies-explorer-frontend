6e8b403c60a59ca9201fae8de9144a9c53d3f1de
Oleg Samoilenko <olaggg-93so@yandex.ru>
2023-05-22 23:18:30 +0300

    deploy

--- src/utils/MainApi.jsx
+++ src/utils/MainApi.jsx
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
-//export const BASE_URL = '';
+//export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "api.oleg021movies.nomoredomains.work";
 
 const checkResponse = (res) => {
   if (res.ok) {
